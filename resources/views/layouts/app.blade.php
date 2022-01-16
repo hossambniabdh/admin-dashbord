b4ae8cf4484723e3f42a1bf58db98a965077038d
hossambniabdh <97850605+hossambniabdh@users.noreply.github.com>
2022-01-16 22:08:06 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,17 +11,10 @@
 
     <!-- Scripts -->
     
-    <script src="{{asset('https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js')}}" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-     
-  
-    <script src="{{asset('//cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js')}}">
-         
-        
-    
-        <link rel="{{asset('//cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css')}}">
-  <script src="{{ asset('js/employee.js') }}" defer></script> 
+   <script src="{{ asset('js/employee.js') }}" defer></script> 
     <script src="{{ asset('js/company.js') }}" defer></script> 
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{asset('https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js')}}" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
